fc6e3c8f212746b06821e21c79e1c235fe9b405f
frfhrts <nikanatroshvili7@gmail.com>
2025-11-13 20:53:57 +0400

    feat: add compose file templates, and implement function to generate them. refactor main.ts code

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,24 @@ import * as fs from "node:fs";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const basePath = join(__dirname, "..", "templates");
+
+const dockerfileList = ["NestJS", "NestJS Microservices", "NextJS"];
+const dockerComposeList = ["base", "dev", "prod"];
+
+const dockerFilesTemplatesPath = join(basePath, "dockerfiles");
+const dockerComposeFilesTemplatesPath = join(basePath, "docker-compose-files");
+
+const dockerfileListString = getListAsString(dockerfileList);
+const dockerComposeFilesListString = getListAsString(dockerComposeList);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 function getListAsString(arrayList: any) {
   return arrayList
     .map((val: string | number, index: number) => {
@@ -10,34 +28,76 @@ function getListAsString(arrayList: any) {
     })
     .join("\n");
 }
-function main() {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
 
-  const dockerfileList = ["NestJS", "NestJS Microservices", "NextJS"];
-  const dockerFilesTemplatesPath = join(__dirname, "..", "templates", "dockerfiles");
+function constructQuestion(questionString: string, options: string[]) {
+  return `\n${questionString}:\n\n${options}\n\nYour Choice:`;
+}
+
+function handleDockerfilesChoice() {
+  rl.question(
+    constructQuestion("Choose Which Dockerfile You Need", dockerfileListString),
+    (answer: string) => {
+      const dockerFilePaths = fs.readdirSync(`${dockerFilesTemplatesPath}`);
+
+      fs.copyFileSync(
+        `${dockerFilesTemplatesPath}/${dockerFilePaths[parseInt(answer) - 1]}`,
+        "./Dockerfile"
+      );
+      rl.close();
+    }
+  );
+}
+
+function handleDockerComposeFilesChoice() {
+  rl.question(
+    constructQuestion(
+      "Choose Which docker compose You Need",
+      dockerComposeFilesListString
+    ),
+    (answer: string) => {
+      const dockerComposeFilePaths = fs.readdirSync(
+        `${dockerComposeFilesTemplatesPath}`
+      );
+      const dockerComposeName = dockerComposeFilePaths[parseInt(answer) - 1];
+
+      fs.copyFileSync(
+        `${dockerComposeFilesTemplatesPath}/${dockerComposeName}`,
+        `./${dockerComposeName}`
+      );
+      rl.close();
+    }
+  );
+}
+
+function main() {
+  const serviceAnswersOptions = ["Dockerfiles", "docker-compose files"];
+
   console.log(`
     ===========================================================\n
     ========================   HERMES  ========================\n
     ===========================================================\n
     `);
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  const listString = getListAsString(dockerfileList);
+  const serviceAnswersOptionsListString = getListAsString(
+    serviceAnswersOptions
+  );
 
   rl.question(
-    `Choose Which Dockerfile You Need:\n\n${listString}\n\nYour choice: `,
-    (answ: string) => {
-      const dockerFilePaths = fs.readdirSync(`${dockerFilesTemplatesPath}`);
-      fs.copyFileSync(
-        `${dockerFilesTemplatesPath}/${dockerFilePaths[parseInt(answ) - 1]}`,
-        "./Dockerfile"
-      );
-      rl.close();
+    constructQuestion(
+      "Choose what you want to setup",
+      serviceAnswersOptionsListString
+    ),
+    (answer: string) => {
+      switch (answer) {
+        case "1":
+          handleDockerfilesChoice();
+          break;
+        case "2":
+          handleDockerComposeFilesChoice();
+          break;
+        default:
+          throw new Error(`Provided Choice '${answer}' Is Incorrect`);
+      }
     }
   );
 }
